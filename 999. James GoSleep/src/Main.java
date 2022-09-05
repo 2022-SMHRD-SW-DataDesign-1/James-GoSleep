7e5849e3bfe7c6a5c9a8b869ad333b9241f2c934
smhrd <smhrd@DESKTOP-1P9A20I>
2022-09-05 16:05:59 +0900

    20220905_고정연: test

--- 999. James GoSleep/src/Main.java
+++ 999. James GoSleep/src/Main.java
@@ -3,24 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
-		System.out.println("git test");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
 	}
 
 }
