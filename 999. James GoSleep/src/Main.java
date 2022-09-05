4a7d11030a2a43b9d82529fa4e26d7a57ca37f63
smhrd <smhrd@DESKTOP-CQ56E2U>
2022-09-05 15:46:10 +0900

    20220905_keonung: conflict!!!

--- 999. James GoSleep/src/Main.java
+++ 999. James GoSleep/src/Main.java
@@ -3,7 +3,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("git test");
-		System.out.println("ÃÖ¿ìÁ¤");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
 	}
 
 }
